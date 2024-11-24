187fe399c5bd01faf80013ab919147a7605546a8
NinePointEightByte <2841154131@qq.com>
2024-11-24 13:24:08 +0800

    Update ICAO.py

--- ICAO.py
+++ ICAO.py
@@ -11,6 +11,7 @@ url = 'https://fsop.caac.gov.cn/g13/jsyzzapp/services/examFace/testScoremesstk?l
 response = requests.get(url, headers={'tk': tk, 'lc': lc})
 
 if response.text == '<ns:testScoremesstkResponse xmlns:ns="http://pepec.icss.com"><ns:return></ns:return></ns:testScoremesstkResponse>':
+    print(sys.getdefaultencoding())
     sys.exit("云执照账号配置不正确")
 
 root = xml.etree.ElementTree.fromstring(response.text)
