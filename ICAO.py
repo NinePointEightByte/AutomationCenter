62e147b29ff2aa76a00ae50e5f5ea23ba583d160
NinePointEightByte <2841154131@qq.com>
2024-11-24 12:30:41 +0800

    Update ICAO.py

--- ICAO.py
+++ ICAO.py
@@ -2,14 +2,15 @@ import json
 import requests
 import xml.etree.ElementTree
 
-lc = '360923200111013114'
-tk = '93891115-eb36-417c-a0ca-d40644d52f5f'
+lc = os.environ.get('lc')
+tk = os.environ.get('tk')
 url = 'https://fsop.caac.gov.cn/g13/jsyzzapp/services/examFace/testScoremesstk?licenum=' + lc + '&messid=' + tk
 
-response = requests.get(url, headers={'tk': tk, 'lc': lc})
+    response = requests.get(url, headers={'tk': tk, 'lc': lc})
 
 if response:
 
+    print(response.text)
     root = xml.etree.ElementTree.fromstring(response.text)
     textReturned = json.loads(root[0].text)['result']
     print(textReturned)
