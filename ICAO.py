10132010a8f6aa3e9f700713cac000864778b215
NinePointEightByte <2841154131@qq.com>
2024-11-25 00:20:35 +0800

    Update ICAO.py

--- ICAO.py
+++ ICAO.py
@@ -28,4 +28,5 @@ wxpusherResponse = requests.post('https://wxpusher.zjiecode.com/api/send/message
 if not wxpusherResponse.json()['code'] == 1000:
     print(wxpusherResponse.text)
 
+print(type(os.environ.get('WXPusherUIDS')))
 print(textReturned)
